c64a0962f1c4ba02b1fd5c6d1077d112736d13bf
andrejsoldatov <andrej.soldatov.88@gmail.com>
2016-05-02 11:54:10 +0400

    Create 12.php

---
+++ php_basics_tasks/12.php
@@ -0,0 +1,12 @@
+<?php
+$day = 5;
+switch ($day) {
+case 1: echo "Понедельник"; break;
+case 2: echo "Вториник"; break;
+case 3: echo "Среда"; break;
+case 4: echo "Четверг"; break;
+case 5: echo "Пятница"; break;
+case 6: echo "Суббота"; break;
+case 7: echo "Воскресенье"; break;
+default: echo "Неизвестный день";
+}
